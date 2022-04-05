49a3b6405a7c7c9537ab45d7de7acf750b818292
oburnett127 <oburnett127@gmail.com>
2022-04-05 14:42:53 -0500

    Been working on job display activity and job feed activity, need to finish new job activity.

--- app/src/main/java/com/example/jobseeker/JobDisplayActivity.java
+++ app/src/main/java/com/example/jobseeker/JobDisplayActivity.java
@@ -1,7 +1,42 @@
 package com.example.jobseeker;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.jobseeker.databinding.ActivityJobDisplayBinding;
 
-public class JobDisplayActivity extends AppCompatActivity {
-    private Job job;
-}
+public class activity_job_display extends AppCompatActivity {
+
+    private ActivityJobDisplayBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityJobDisplayBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+//
+//        setSupportActionBar(binding.toolbar);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_activity_job_display);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_activity_job_display);
+//        return NavigationUI.navigateUp(navController, appBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+//}
--- app/src/main/java/com/example/jobseeker/JobFeedActivity.java
+++ app/src/main/java/com/example/jobseeker/JobFeedActivity.java
@@ -39,27 +39,30 @@ public class JobFeedActivity extends AppCompatActivity implements JobRVAdapter.O
 
         jobViewModel.getAllJobs().observe(this, jobs -> {
             jobRVAdapter = new JobRVAdapter(jobs, JobFeedActivity.this, this);
-            jobRV.setAdapter(jobRVAdapter)
-                .setOnItemClickListener(new JobRVAdapter.OnItemClickListener() {
-                    @Override
-                    public void onJobFeedItemClick(Job job) {
-                            Intent intent = new Intent(JobFeedActivity.this, JobDisplayActivity.class);
-                            intent.putExtra(JobDisplayActivity., job.getId());
-                            intent.putExtra(JobDisplayActivity.JOB_TITLE, job.getTitle());
-                            intent.putExtra(JobDisplayActivity.JOB_EMPLOYER, job.getEmployer());
-                            intent.putExtra(JobDisplayActivity.JOB_DESC, job.getDesc());
-                            startActivityForResult(intent);
-                    }
-                });
+            jobRV.setAdapter(jobRVAdapter);
+//                .setOnItemClickListener(new JobRVAdapter.OnItemClickListener() {
+//                    @Override
+//                    public void onJobFeedItemClick(Job job) {
+//                            Intent intent = new Intent(JobFeedActivity.this, JobDisplayActivity.class);
+//                            intent.putExtra(JobDisplayActivity.JOB_ID, job.getId());
+//                            intent.putExtra(JobDisplayActivity.JOB_TITLE, job.getTitle());
+//                            intent.putExtra(JobDisplayActivity.JOB_EMPLOYER, job.getEmployer());
+//                            intent.putExtra(JobDisplayActivity.JOB_DESC, job.getDesc());
+//                            startActivityForResult(intent);
+//                    }
+//                });
+        });
     }
 
-//    @Override
-//    public void onJobClick(int position) {
-//        Job job = Objects.requireNonNull(jobViewModel.getAllJobs().getValue()).get(position);
-//        //Log.d(TAG, "onJobClick: " + job.getId());
-//
-//        Intent intent = new Intent(MainActivity.this, JobDisplayActivity.class);
-//        intent.putExtra(JOB_ID, job.getId());
-//        startActivity(intent);
+    public void onJobClick(int position) {
+        Job job = Objects.requireNonNull(jobViewModel.getAllJobs().getValue()).get(position);
+        //Log.d(TAG, "onJobClick: " + job.getId());
+
+        Intent intent = new Intent(JobFeedActivity.this, JobDisplayActivity.class);
+        intent.putExtra(JOB_ID, job.getId());
+//        intent.putExtra(JobDisplayActivity.JOB_TITLE, job.getTitle());
+//        intent.putExtra(JobDisplayActivity.JOB_EMPLOYER, job.getEmployer());
+//        intent.putExtra(JobDisplayActivity.JOB_DESC, job.getDesc());
+        startActivity(intent);
     }
 }
